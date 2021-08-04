0e38f8f6a7c683c859b66f7fca3e5402a77bdde5
AliceDelpuppo <alicedelpuppo1@gmail.com>
2021-08-03 22:14:33 -0300

    Início JS dos botões

---
+++ script.js
@@ -0,0 +1,38 @@
+const $buttonStone1 = document.querySelector('#button-strone-p1')
+const $buttonPaper1 = document.querySelector('#button-paper-p1')
+const $buttonScissor1 = document.querySelector('#button-scissor-p1')
+
+const $buttonStone2 = document.querySelector('#button-stone-p2')
+const $buttonPaper2 = document.querySelector('#button-paper-p2')
+const $buttonScissor2 = document.querySelector('#button-scissor-p2')
+
+const $fieldPlayer1 = document.querySelector('.player1-field')
+const $fieldPlayer2 = document.querySelector('.player2-field')
+
+console.log($fieldPlayer1)
+console.log($fieldPlayer2)
+
+$buttonStone1.addEventListener('click', function(){
+    $fieldPlayer1.innerHTML = '<img src="image/stone.png" class="image">'
+})
+
+$buttonPaper1.addEventListener('click', function(){
+    $fieldPlayer1.innerHTML = '<img src="image/paper.png" class="image">'
+})
+
+$buttonScissor1.addEventListener('click', function(){
+    $fieldPlayer1.innerHTML = '<img src="image/scissors.png" class="image">'
+})
+
+$buttonStone2.addEventListener('click', function(){
+    $fieldPlayer2.innerHTML = '<img src="image/stone.png" class="image">'
+})
+
+$buttonPaper2.addEventListener('click', function(){
+    $fieldPlayer2.innerHTML = '<img src="image/paper.png" class="image">'
+})
+
+$buttonScissor2.addEventListener('click', function(){
+    $fieldPlayer2.innerHTML = '<img src="image/scissors.png" class="image">'
+})
+
